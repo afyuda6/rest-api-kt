3762b1bddcdc12075f9bd46848033e9c2738210f
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-22 00:12:59 +0700

    22/12/2024

--- Main.kt
+++ Main.kt
@@ -3,8 +3,9 @@ import handlers.UserHandler
 import java.net.InetSocketAddress
 
 fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 6009
     connectToDatabase()
-    val server = HttpServer.create(InetSocketAddress(6009), 0)
+    val server = HttpServer.create(InetSocketAddress(port), 0)
     server.createContext("/") { exchange ->
         UserHandler().handle(exchange);
     }
